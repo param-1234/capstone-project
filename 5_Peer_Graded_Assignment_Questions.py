ed11d2ea984308979ee3d88cd1e815244006ca00
param-1234 <130355687+param-1234@users.noreply.github.com>
2023-04-14 16:35:34 +0530

    Add files via upload

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -132,7 +132,7 @@ app.layout = html.Div(children=[
                                         html.Div([ ], id='plot5')
                                 ], style={'display': 'flex'}),
                                 ])
-                            
+                                
 
 # Callback function definition
 # TASK4: Add 5 ouput components
@@ -142,8 +142,6 @@ app.layout = html.Div(children=[
                 Output(component_id='plot3', component_property='children'),
                 Output(component_id='plot4', component_property='children'),
                 Output(component_id='plot5', component_property='children')],
-               [Input(component_id='input-type', component_property='value'),
-                Input(component_id='input-year', component_property='value')],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
@@ -166,7 +164,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            figure_name = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline',  title ='Average monthly flight time (minutes) by airline')
+            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline',  title ='Average monthly flight time (minutes) by airline')
             
             
             # Percentage of diverted airport landings per reporting airline
@@ -192,7 +190,6 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
                       color_continuous_scale='RdBu',
                       title='Flight count by airline to destination state'
                 )
-            
             
             
             # REVIEW6: Return dcc.Graph component to the empty division
